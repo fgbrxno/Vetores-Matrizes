5abde1584d7c756d5eb2919f99a511ea54036991
Bruno Felipe <brunofg666@gmail.com>
2025-09-16 19:43:08 -0300

    feat: adicionada leitura de 10 números pelo usuário

--- src/atividades/MaiorMenorNumero.java
+++ src/atividades/MaiorMenorNumero.java
@@ -1,4 +1,17 @@
 package atividades;
 
+import java.util.Scanner;
+
 public class MaiorMenorNumero {
-}
+
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+
+        int[] numeros = new int[10];
+
+        // Laço para ler os 10 números do usuário
+        for (int i = 0; i < numeros.length; i++) {
+            System.out.print("Digite o " + (i + 1) + "º número: ");
+            numeros[i] = scanner.nextInt(); // Lê um número inteiro e armazena no vetor
+        }
