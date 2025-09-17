447f8b1484848e8bb8ead7afe1480f4cb8244fda
Bruno Felipe <brunofg666@gmail.com>
2025-09-17 11:06:47 -0300

    feat: finalizada lógica para identificar maior e menor número

--- src/atividades/MaiorMenorNumero.java
+++ src/atividades/MaiorMenorNumero.java
@@ -15,3 +15,22 @@ public class MaiorMenorNumero {
             System.out.print("Digite o " + (i + 1) + "º número: ");
             numeros[i] = scanner.nextInt(); // Lê um número inteiro e armazena no vetor
         }
+
+        // Variáveis para armazenar o maior e o menor número
+        int maior = numeros[0];
+        int menor = numeros[0];
+
+        // Laço para percorrer o vetor e encontrar  o maior e menor
+        for (int i = 1; i < numeros.length; i++) {
+            if (numeros[i] > maior) {
+                maior = numeros[i];
+            }
+            if (numeros[i] < menor) {
+                menor = numeros[i];
+            }
+        }
+
+        System.out.println("Maior número: " + maior);
+        System.out.println("Menor número: " + menor);
+    }
+}
