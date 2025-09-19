6316809b4aa844c78405611f0190bc9c8f529833
Bruno Felipe <brunofg666@gmail.com>
2025-09-19 13:26:02 -0300

    feat: adicionada matriz 4x4 e exibição da diagonal principal

--- src/atividades/DiagonalPrincipal.java
+++ src/atividades/DiagonalPrincipal.java
@@ -1,4 +1,23 @@
 package atividades;
 
 public class DiagonalPrincipal {
+
+    public static void main(String[] args) {
+
+        int[][] matriz = new int[4][4]; // cria a matriz 4x4
+
+        // Preenche a matriz com valores
+        int contador = 1;
+        for (int i = 0; i < 4; i++) {
+            for (int j = 0; j < 4; j++) {
+                matriz[i][j] = contador++;
+            }
+        }
+
+        // Mostra os elementos da diagonal principal
+        System.out.println("Diagonal principal:");
+        for (int i = 0; i < 4; i++) {
+            System.out.println(matriz[i][i]);
+        }
+    }
 }
