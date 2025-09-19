7a110e576b990dc20f42d34d83f94f761b3806b6
Bruno Felipe <brunofg666@gmail.com>
2025-09-19 09:56:12 -0300

    feat: adicionado leitor de valores por linha

--- src/atividades/Matriz3x3.java
+++ src/atividades/Matriz3x3.java
@@ -22,5 +22,16 @@ public class Matriz3x3 {
                 // Repete novamente até i atingir a quantia esperada de repetições;
             }
         }
+
+        // Mostrar a matriz no formato tradicional
+        System.out.println("Matriz 3x3:");
+        for (int i = 0; i < 3; i++) { // percorre as linhas
+            for (int j = 0; j < 3; j++) { // percorre as colunas
+                System.out.print(matriz[i][j] + " "); // imprime o elemento da linha i e coluna j
+            }
+            System.out.println(); // pula para a próxima linha
+        }
+
+        scanner.close();
     }
 }
