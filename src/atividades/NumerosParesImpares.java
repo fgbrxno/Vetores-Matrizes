25cb00a491b73c77f99dcfef86a3882e592c2d69
Bruno Felipe <brunofg666@gmail.com>
2025-09-19 08:57:42 -0300

    feat: adicionado separador de números pares e ímpares, se o número for divisivel por 2 e resto for 0, então é par; do contrário o resultado é ímpar

--- src/atividades/NumerosParesImpares.java
+++ src/atividades/NumerosParesImpares.java
@@ -6,11 +6,22 @@ public class NumerosParesImpares {
 
         int[] numeros = new int[15];
 
-        // Preencher o vetor com números aleatórios de 1 a 100
+        // Preenche o vetor com números aleatórios de 1 a 100
         for (int i = 0; i < numeros.length; i++) {
             numeros[i] = (int) (Math.random() * 100) + 1;
             // Math.random() gera um número entre 0.0 e 1.0, depois é multiplicado por 100 e somado 1, resultando = 1 a 100
             // int muda o valor de decimal para inteiro
         }
+        // Faz a separação entre numeros pares e ímpares
+        for (int i = 0; i < numeros.length; i++) {
+            if (numeros[i] % 2 == 0) {
+                System.out.println(numeros[i] + " é par");
+            } else {
+                System.out.println(numeros[i] + " é ímpar");
+                // Se divisivel por 2 e resto = 0, então é par
+                // Do contrario é ímpar
+            }
+        }
+
     }
 }
