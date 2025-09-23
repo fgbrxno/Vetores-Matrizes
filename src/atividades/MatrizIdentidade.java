a448c7dd94254faff93033d6f8b5992ab0de6eb1
Bruno Felipe <brunofg666@gmail.com>
2025-09-23 08:16:57 -0300

    feat: criada a matriz identidade 5x5 com 1 na diagonal e 0 nos outros lugares

--- src/atividades/MatrizIdentidade.java
+++ src/atividades/MatrizIdentidade.java
@@ -1,4 +1,20 @@
 package atividades;
 
 public class MatrizIdentidade {
+
+    public static void main(String[] args) {
+
+        int[][] identidade = new int[5][5];
+        for (int i = 0; i < 5; i++) {
+            identidade[i][i] = 1;
+        }
+
+        System.out.println("Matriz Identidade 5x5:");
+        for (int[] linha : identidade) {
+            for (int valor : linha) {
+                System.out.print(valor + " ");
+            }
+            System.out.println();
+        }
+    }
 }
