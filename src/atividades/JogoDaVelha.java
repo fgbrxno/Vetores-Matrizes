56eedc048db76f988ca6563d0060bf1f586f1dc5
Bruno Felipe <brunofg666@gmail.com>
2025-09-23 15:03:36 -0300

    feat: criada lógica inicial do jogo da velha com matriz 3x3 e jogadas alternadas de X e O

--- src/atividades/JogoDaVelha.java
+++ src/atividades/JogoDaVelha.java
@@ -1,6 +1,6 @@
 package atividades;
 
-
+// Commit: Criada lógica inicial do jogo da velha
 
 import java.util.Scanner;
 
@@ -14,5 +14,29 @@ public class JogoDaVelha {
                 tabuleiro[i][j] = '-';
             }
         }
+
+        boolean vezDoX = true;
+        for (int rodada = 0; rodada < 5; rodada++) {
+            System.out.println("Tabuleiro:");
+            for (char[] linha : tabuleiro) {
+                for (char valor : linha) {
+                    System.out.print(valor + " ");
+                }
+                System.out.println();
+            }
+
+            System.out.println("Digite linha (0-2) e coluna (0-2):");
+            int l = sc.nextInt();
+            int c = sc.nextInt();
+
+            if (tabuleiro[l][c] == '-') {
+                tabuleiro[l][c] = vezDoX ? 'X' : 'O';
+                vezDoX = !vezDoX;
+            } else {
+                System.out.println("Ja foi bobão, tenta outro.");
+                rodada--;
+            }
+        }
+        sc.close();
     }
 }
