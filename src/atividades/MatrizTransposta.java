7ed1dd4dd96a5cb3b11535f1e10b58f44fd2268c
Bruno Felipe <brunofg666@gmail.com>
2025-09-23 08:28:19 -0300

    implementada matriz 3x2 e sua transposta 2x3 com exibição formatada

--- src/atividades/MatrizTransposta.java
+++ src/atividades/MatrizTransposta.java
@@ -1,4 +1,24 @@
 package atividades;
 
+// Commit: Criada a matriz 3x2 e exibida sua transposta
+
 public class MatrizTransposta {
+    public static void main(String[] args) {
+        int[][] matriz3x2 = { {1, 2}, {3, 4}, {5, 6} };
+        int[][] transposta = new int[2][3];
+
+        for (int i = 0; i < 3; i++) {
+            for (int j = 0; j < 2; j++) {
+                transposta[j][i] = matriz3x2[i][j];
+            }
+        }
+
+        System.out.println("Matriz Transposta 2x3:");
+        for (int[] linha : transposta) {
+            for (int valor : linha) {
+                System.out.print(valor + " ");
+            }
+            System.out.println();
+        }
+    }
 }
