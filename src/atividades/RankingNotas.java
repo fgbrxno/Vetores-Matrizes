8ac65fca9c9a6a1524791e0a11598ff9487b0ff5
Bruno Felipe <brunofg666@gmail.com>
2025-09-24 13:24:27 -0300

    feat: criado ranking de notas e cálculo de médias

--- src/atividades/RankingNotas.java
+++ src/atividades/RankingNotas.java
@@ -1,4 +1,20 @@
 package atividades;
 
 public class RankingNotas {
+    public static void main(String[] args) {
+        double[][] notas = {
+                {7.5, 8.0},
+                {6.0, 9.0},
+                {5.5, 4.0},
+                {10.0, 9.5},
+                {8.5, 7.0}
+        };
+
+        double[] medias = new double[5];
+        System.out.println("Ranking de Notas:");
+        for (int i = 0; i < 5; i++) {
+            medias[i] = (notas[i][0] + notas[i][1]) / 2.0;
+            System.out.println("Aluno " + (i + 1) + ": Média = " + medias[i]);
+        }
+    }
 }
