90bd63a476298997fea219128b604ce7b3a4bbdc
Bruno Felipe <brunofg666@gmail.com>
2025-09-24 13:30:20 -0300

    feat: criada matriz do mar 7x7 preenchida com '~'

--- src/atividades/BatalhaNaval.java
+++ src/atividades/BatalhaNaval.java
@@ -1,4 +1,21 @@
 package atividades;
 
+import java.util.Random;
+import java.util.Scanner;
+
 public class BatalhaNaval {
-}
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        Random random = new Random();
+
+
+        char[][] mar = new char[7][7];
+        for (int i = 0; i < 7; i++) {
+            for (int j = 0; j < 7; j++) {
+                mar[i][j] = '~';
+                }
+            }
+        }
+    }
+
