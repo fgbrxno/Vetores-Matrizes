54f4fbd064cfc33bb37cee88ab4f5baed39f233d
Bruno Felipe <brunofg666@gmail.com>
2025-09-24 14:07:08 -0300

    feat: adicionado verificação de erro e acerto e exibição do resultado

--- src/atividades/BatalhaNaval.java
+++ src/atividades/BatalhaNaval.java
@@ -4,18 +4,16 @@ import java.util.Random;
 import java.util.Scanner;
 
 public class BatalhaNaval {
-
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         Random random = new Random();
-
 
         char[][] mar = new char[7][7];
         for (int i = 0; i < 7; i++) {
             for (int j = 0; j < 7; j++) {
                 mar[i][j] = '~';
-                }
             }
+        }
 
         boolean[][] navios = new boolean[7][7];
         int naviosRestantes = 3;
@@ -31,10 +29,29 @@ public class BatalhaNaval {
 
         System.out.println("Batalha Naval:");
         while (naviosRestantes > 0) {
-            System.out.println("Digite linha (0-6) e coluna (0-6):");
+            System.out.println("ajude o corinthians a derrubar os rivais, digite linha (0-6) e coluna (0-6):");
             int l = sc.nextInt();
             int c = sc.nextInt();
+
+            if (navios[l][c]) {
+                System.out.println("acertou, vai corinthians");
+                mar[l][c] = 'X';
+                navios[l][c] = false;
+                naviosRestantes--;
+            } else {
+                System.out.println("só água :(");
+                mar[l][c] = 'O';
+            }
+
+            for (char[] linha : mar) {
+                for (char valor : linha) {
+                    System.out.print(valor + " ");
+                }
+                System.out.println();
+            }
         }
+
+        System.out.println("todos os navios foram afundados, e o corinthians ganhou mais um mundial");
+        sc.close();
     }
 }
-
